7de879d89690266d1dc2b75c74fb606c8caf52f9
Gavin Kimberlin <guviner@gmail.com>
2020-08-10 10:58:53 -0700

    Create a program using datetime and exception handling.

---
+++ CS_Step234/LessThanOneException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Step234
+{
+    class LessThanOneException : Exception
+    {
+        public LessThanOneException()
+    : base() { }
+        public LessThanOneException(string message)
+            : base(message) { }
+    }
+}
--- CS_Step234/Program.cs
+++ CS_Step234/Program.cs
@@ -10,6 +10,26 @@ namespace CS_Step234
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What is your age?");
+            try
+            {
+                int age = Convert.ToInt32(Console.ReadLine());
+                if (age < 1)
+                {
+                    throw new LessThanOneException();
+                }
+                int yearBorn = DateTime.Now.Year - age;
+                Console.WriteLine("You were born in " + yearBorn);
+            }
+            catch (LessThanOneException)
+            {
+                Console.WriteLine("Please enter an age greater than one.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter a valid age");
+            }
+            Console.ReadLine();
         }
     }
 }
